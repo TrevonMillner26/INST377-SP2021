44b705660100bf0e568d206720546c601186f293
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-02-12 14:18:47 -0500

    Fixed: arrow variables, started arrow constants, took out exces js code

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,3 +1,8 @@
 /* Put your javascript in here */
-
+function clickArrowLeft() {
+    document.addEventListener('click', (event) => {
+        const arrow_left = document.querySelector("#arrow_left");
+        const classes = arrow_left
+    }
+}
 
