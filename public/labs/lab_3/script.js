0b0a4957a3433c860a2c64590c506d50ea1cd9c7
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-02-17 18:38:45 -0500

    Added: function clickArrowLeft

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -2,7 +2,8 @@
 function clickArrowLeft() {
     document.addEventListener('click', (event) => {
         const arrow_left = document.querySelector("#arrow_left");
-        const classes = arrow_left
+        const classes = arrow_left.classList;
+        console.log(classes)
     }
 }
 
