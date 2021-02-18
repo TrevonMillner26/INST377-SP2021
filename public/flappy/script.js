807634d1f125a65f6261b1c95262416d22f443a6
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-02-18 13:45:00 -0500

    Added: randomHeight

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -31,7 +31,8 @@ document.addEventListener('DOMContentLoaded' , () => {
 
     function generateObstacle() {
         let obstacleLeft = 500
-        let obstacleBottom = 150
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
         obstacle.classList.add('obstacle')
         gameDisplay.appendChild(obstacle)
