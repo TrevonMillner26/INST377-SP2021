40cc5e3449c928a1ffef9cb7f549815e4ee5cb54
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-02-18 15:08:59 -0500

    Add & Update: Downloaded pipe

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -49,7 +49,8 @@ document.addEventListener('DOMContentLoaded' , () => {
                 gameDisplay.removeChild(obstacle)
             }
             if (
-                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 
+                birdBottom < obstacleBottom + 153||
                 birdBottom === 0
                 ) {
                 gameOver()
