8c75d96d65725800d903a509233b3cf1ae3a77d2
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-03-31 20:10:00 -0400

    Creating new platform classes

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
     let isGameOver = false
+    let platformCount = 5
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -15,11 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     }
+
+    class Platform {
+        constructor(newPlatBottom) {
+            
+        }
+    }
+
+    function createPlatforms() {
+        for (let i =0; i < platformCount; i++) {
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform(newPlatBottom)
+        }
+    }
     
 
     function start() {
         if(!isGameOver) {
             createDoodler()
+            createPlatforms()
         }
     }
     // attach to button
